e80fbcf55aa19651b3c5ce23b0bd302157d399e0
FortPercent <fortpercent@users.noreply.github.com>
2025-05-09 08:13:57 +0000

    change grad dtype to bf16 and convert to fp32 when allreduce

--- teletron/megatron_adaptor.py
+++ teletron/megatron_adaptor.py
@@ -4,6 +4,7 @@ import megatron.core
 from teletron.core.parallel_state import initialize_model_parallel_decorators
 from teletron.core.training import setup_model_and_optimizer_decorators
 from teletron.core.distributed.distributed_data_parallel import DistributedDataParallel
+from teletron.core.distributed.param_and_grad_buffer import start_grad_sync
 
 def exe_adaptation():
     megatron.core.parallel_state.initialize_model_parallel = initialize_model_parallel_decorators(
@@ -16,5 +17,6 @@ def exe_adaptation():
     )
 
     megatron.core.distributed.DistributedDataParallel = DistributedDataParallel
+    megatron.core.distributed.param_and_grad_buffer.ParamAndGradBuffer.start_grad_sync = start_grad_sync
     
 exe_adaptation()
