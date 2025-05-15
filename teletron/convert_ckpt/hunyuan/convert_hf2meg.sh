388622d5cf33448efa7a2a40063f422009deb9c8
lit117 <lit117@chinatelecom.cn>
2025-05-15 07:53:34 +0000

    convert split into 2 shells

--- teletron/convert_ckpt/hunyuan/convert_hf2meg.sh
+++ teletron/convert_ckpt/hunyuan/convert_hf2meg.sh
@@ -12,11 +12,11 @@ HUGGINGFACE_CKPT_PATH="/nvfile-heatstorage/model_zoo/huggingface/hunyuan/hunyuan
 # SOURCE_CKPT_PATH="/nvfile-heatstorage/teleai-infra/litian/megatron_ckpt/ckpt_tp1_2040_linearparallel_epoch1step2700/iter_0002400"
 # TARGET_CKPT_PATH="/nvfile-heatstorage/teleai-infra/litian/megatron_ckpt/vast_ckpt/tp1_iter2400"
 SOURCE_CKPT_PATH="/nvfile-heatstorage/ljq/repos/vast/work_dirs/hunyuanvideo_i2vhy_newdataset_720p_1e5_spring_newdata_0210/models/checkpoint_epoch_1_step_2700/transformer_safetensor"
-TARGET_CKPT_PATH="/workspace/ckpt_tp2_36_2700/"
+TARGET_CKPT_PATH="/workspace/ckpt_tp2_2040_2700test/"
 TP=2
 PP=1
 
-python convert_hunyuan_new.py  \
+python convert_hunyuan.py  \
     --load ${SOURCE_CKPT_PATH} \
     --save ${TARGET_CKPT_PATH} \
     --hf-ckpt-path ${HUGGINGFACE_CKPT_PATH} \
