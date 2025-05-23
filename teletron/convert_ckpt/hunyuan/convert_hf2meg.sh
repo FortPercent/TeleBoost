aa2b3a1b5cd51b226d10b002d738f1bed4e43200
FortPercent <fortpercent@users.noreply.github.com>
2025-05-23 09:58:13 +0800

    read model path from json config

--- teletron/convert_ckpt/hunyuan/convert_hf2meg.sh
+++ teletron/convert_ckpt/hunyuan/convert_hf2meg.sh
@@ -1,15 +1,20 @@
-export PYTHONPATH=$PYTHONPATH:/path/to/Megatron-LM
+export PYTHONPATH=$PYTHONPATH:/nvfile-heatstorage/yxy/code/Megatron_VAST
 
 # Path to the HuggingFace checkpoint and its transformer directory
-HUGGINGFACE_CKPT_PATH="/path/to/huggingface/hunyuanvideo"
-SOURCE_CKPT_PATH="${HUGGINGFACE_CKPT_PATH}/transformer"
+CKPT_NAME="hunyuanvideo_i2vhy_token_replace"
+#CKPT_NAME="hunyuanvideo_i2v_multimask"
 
-# Output path for the converted Megatron-style checkpoint
-TARGET_CKPT_PATH="/path/to/output/teletron_checkpoint/"
+HUGGINGFACE_CKPT_PATH="/nvfile-heatstorage/model_zoo/huggingface/hunyuan/hunyuanvideo_13b"
+CONFIG_FILE="/nvfile-heatstorage/yxy/code/Teletron/model_paths.json"
+CKPT_PATH=$(jq -r ".${CKPT_NAME}" "$CONFIG_FILE")
+SOURCE_CKPT_PATH="${CKPT_PATH}/transformer_safetensor"
+TARGET_CKPT_PATH="${CKPT_PATH}/teletron"
 
 # Model parallelism settings
 TP=1  # Tensor Parallelism
 PP=1  # Pipeline Parallelism
+
+rm -r $TARGET_CKPT_PATH
 
 # Run the conversion script
 python convert_hunyuanvideo.py  \
