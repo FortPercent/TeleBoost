888b80c33295c75e2700aa8af00a406cc3540cb6
yangxy57 <1524552292@qq.com>
2025-05-28 15:33:25 +0800

    broadcast list[str] by _broadcast_object_list

--- teletron/training/utils.py
+++ teletron/training/utils.py
@@ -8,6 +8,7 @@ from megatron.training import (
     get_args,
 )
 from megatron.core import mpu
+from typing import get_origin
 
 
 def get_batch_on_this_tp_rank_vast(data_iterator):
@@ -63,48 +64,80 @@ def get_batch_on_this_tp_rank_vast(data_iterator):
 
 def get_batch_on_this_tp_cp_rank_vast(data_iterator):
     args = get_args()
-    data_dict = ['images', 'prompt_embeds', 'prompt_masks', 'clip_text_embed', 'ref_mask', 'ref_images']
+    data_dict = {
+        'images': torch.float32,
+        'prompt_embeds': torch.float32,
+        'prompt_masks': torch.int64,
+        'clip_text_embed': torch.float32,
+        'ref_mask': torch.float32,
+        'ref_images': torch.float32,
+        'struct_prompt': list[str],
+        'short_prompt': list[str],
+        'dense_prompt': list[str],
+    }
 
     def _broadcast(item):
         if item is not None:
            torch.distributed.broadcast(item, mpu.get_tensor_context_parallel_src_rank(), group=mpu.get_tensor_context_parallel_group())
+
+    def _broadcast_object_list(item):
+        if item is not None:
+           torch.distributed.broadcast_object_list(item, mpu.get_tensor_context_parallel_src_rank(), group=mpu.get_tensor_context_parallel_group())
     
     if mpu.get_tensor_context_parallel_rank() == 0:
         if data_iterator is not None:
            data = next(data_iterator)
         else:
            data = None
+        assert all(key in data_dict for key in data.keys()), f"Not all keys from input valid: {set(data.keys()) - set(data_dict)}"
+        
         batch = {}
-        data_dict = [d for d in data.keys()]
-        for param in data_dict:
-            if isinstance(data[param], list): # prompt is list
-                pass
-            elif isinstance(data[param], torch.Tensor):
+        for param in data.keys():
+            dtype = data_dict[param]
+            if get_origin(dtype) is list:
+                assert isinstance(data[param], list), f"{param} is not list"
+                # 字符串列表更新内容本身
+                batch.update({param: data[param]})
+            elif isinstance(dtype, torch.dtype):
+                assert data[param].dtype == dtype, f"{param} is not of type {dtype}"
+                # torch Tensor更新前先传输到CUDA
                 batch.update({param: data[param].cuda(non_blocking = True)})
             else:
-                raise NotImplementedError(f"Unsupported data type: {type(data[param])}")
+                raise NotImplementedError(f"Unsupported data type: {type(data[param]), dtype}")
 
         # Step 1: 保存每部分的大小信息（只在 Rank 0 执行）
-        sizes_info = {key: tensor.size() if tensor is not None else None for key, tensor in batch.items()}
+        sizes_info = {key: tensor.size() if (tensor is not None and isinstance(tensor, torch.Tensor)) else None for key, tensor in batch.items()}
         # Step 2: 广播大小信息
-        sizes_info = torch.distributed.broadcast_object_list([sizes_info],mpu.get_tensor_context_parallel_src_rank(), group=mpu.get_tensor_context_parallel_group())
-        
-        for param in batch.keys():
-            _broadcast(batch[param])
+        _broadcast_object_list([sizes_info])
+        for param in data.keys():
+            dtype = data_dict[param]
+            if get_origin(dtype) is list: 
+                # 字符串以object list的形式广播
+                _broadcast_object_list(batch[param])
+            elif isinstance(dtype, torch.dtype):
+                _broadcast(batch[param])
 
     else:
+        # TODO check 
         sizes_info = None 
         sizes_info_list = [sizes_info]
-        torch.distributed.broadcast_object_list(sizes_info_list,mpu.get_tensor_context_parallel_src_rank(), group=mpu.get_tensor_context_parallel_group())
+        _broadcast_object_list(sizes_info_list)
 
         batch = {}
-        data_dict = [d for d in sizes_info_list[0].keys()]
-        for param in data_dict:
-            if param == "prompt_masks":
-                batch. update({param: torch.empty(sizes_info_list[0][param], dtype=torch.int64, device=torch.cuda.current_device())})
-            else:
-                batch. update({param: torch.empty(sizes_info_list[0][param], dtype=torch.float32, device=torch.cuda.current_device())})
+        for param in sizes_info_list[0].keys():
+            dtype = data_dict[param]
+            if get_origin(dtype) is list: 
+                # 需要注意，列表需要有一个默认值占位，否则广播失败
+                batch.update({param: ['']})
+            elif isinstance(dtype, torch.dtype):
+                batch.update({param: torch.empty(sizes_info_list[0][param], dtype=dtype, device=torch.cuda.current_device())})
+
         for param in batch.keys():
-            _broadcast(batch[param])
+            dtype = data_dict[param]
+            if get_origin(dtype) is list: 
+                # 以object list的形式广播字符串列表
+                _broadcast_object_list(batch[param])
+            elif isinstance(dtype, torch.dtype):
+                _broadcast(batch[param])
 
     return batch
