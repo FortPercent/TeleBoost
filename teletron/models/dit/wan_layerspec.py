6ddd36db885067cbd9303efa7f838c65dd804d4e
FortPercent <fortpercent@users.noreply.github.com>
2025-06-04 13:46:47 +0800

    copy file from megatron_vast

---
+++ teletron/models/dit/wan_layerspec.py
@@ -0,0 +1,246 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import copy
+from dataclasses import dataclass
+from typing import Literal, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
+import torch
+import torch.nn as nn
+from einops import rearrange
+from megatron.core.jit import jit_fuser
+from megatron.core.transformer.attention import (
+    CrossAttention,
+    CrossAttentionSubmodules,
+    SelfAttention,
+    SelfAttentionSubmodules,
+)
+from megatron.core import mpu
+from megatron.core.fusions.fused_bias_dropout import get_bias_dropout_add
+from megatron.core.extensions.transformer_engine import (
+    TEColumnParallelLinear,
+    TEDotProductAttention,
+    TENorm,
+    TERowParallelLinear,
+)
+from diffusers.models.normalization import (
+    AdaLayerNormContinuous,
+    AdaLayerNormZero,
+    AdaLayerNormZeroSingle,
+)
+from megatron.core.transformer.utils import sharded_state_dict_default
+from megatron.core.dist_checkpointing.utils import replace_prefix_for_sharding
+from megatron.core.dist_checkpointing.mapping import ShardedStateDict
+from megatron.core.transformer.enums import AttnMaskType
+from megatron.core.transformer.identity_op import IdentityOp
+from megatron.core.transformer.mlp import MLP, MLPSubmodules
+from megatron.core.transformer.module import MegatronModule
+from megatron.core.transformer.spec_utils import ModuleSpec, build_module
+from megatron.core.transformer.transformer_block import TransformerConfig
+from megatron.core.transformer.transformer_config import TransformerConfig
+from megatron.core.transformer.transformer_layer import TransformerLayer, TransformerLayerSubmodules
+from megatron.core.utils import make_viewless_tensor
+
+from megatron.core.models.dit.wan_attention import (
+    WanCrossAttention,
+    WanSelfAttention,
+    WanCrossAttentionSubmodules
+)
+
+from diffusers.models.normalization import FP32LayerNorm
+from diffusers.models.attention import FeedForward
+from megatron.core.tensor_parallel.mappings import (
+    gather_from_tensor_model_parallel_region,
+)
+
+from megatron.core.transformer.dot_product_attention import DotProductAttention
+from megatron.core.tensor_parallel.layers import ColumnParallelLinear, RowParallelLinear
+
+class RMSNorm(nn.Module):
+    def __init__(self, hidden_size: int, config, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight
+
+
+class WanDiTLayer(TransformerLayer):
+    """A double transformer layer.
+
+    Transformer layer takes input with size [s, b, h] and returns an
+    output of the same size.
+
+    WanDiT layer implementation from [https://arxiv.org/pdf/2403.03206].
+    """
+    def __init__(
+        self,
+        config: TransformerConfig,
+        submodules: TransformerLayerSubmodules,
+        layer_number: int = 1,
+        eps: float= 1e-6,
+        cross_attn_norm: bool = True,
+    ):
+        hidden_size = config.hidden_size
+        super().__init__(config=config, submodules=submodules, layer_number=layer_number)
+
+        self.norm1 = FP32LayerNorm(hidden_size, eps, elementwise_affine=False)
+        self.norm2 = FP32LayerNorm(hidden_size, eps, elementwise_affine=True) if cross_attn_norm else nn.Identity()
+
+        # 3. Feed-forward
+        # self.ffn = FeedForward(hidden_size, inner_dim=config.ffn_dim, activation_fn="gelu-approximate")
+        self.norm3 = FP32LayerNorm(hidden_size, eps, elementwise_affine=False)
+
+        self.scale_shift_table = nn.Parameter(torch.randn(1, 6, hidden_size) / hidden_size**0.5)
+
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        encoder_hidden_states: torch.Tensor,
+        temb: torch.Tensor,
+        rotary_emb: torch.Tensor,
+    ):
+        # 1. Input normalization
+        shift_msa, scale_msa, gate_msa, c_shift_msa, c_scale_msa, c_gate_msa = (
+            self.scale_shift_table + temb.float()
+        ).chunk(6, dim=1)
+
+        # 1. Self-attention
+        norm_hidden_states = (self.norm1(hidden_states.float()) * (1 + scale_msa) + shift_msa).type_as(hidden_states)
+        attn_output = self.self_attention(hidden_states=norm_hidden_states, 
+                                          rotary_pos_emb=rotary_emb,
+                                          attention_mask=None)
+        hidden_states = (hidden_states.float() + attn_output * gate_msa).type_as(hidden_states)
+        # 2. Cross-attention
+        norm_hidden_states = self.norm2(hidden_states.float()).type_as(hidden_states)
+        attn_output = self.cross_attention(hidden_states=norm_hidden_states, 
+                                           encoder_hidden_states=encoder_hidden_states,
+                                           attention_mask=None)
+        hidden_states = hidden_states + attn_output
+
+        # 3. Feed-forward
+        norm_hidden_states = (self.norm3(hidden_states.float()) * (1 + c_scale_msa) + c_shift_msa).type_as(
+            hidden_states
+        )
+        ff_output,bias = self.mlp(norm_hidden_states)
+        ff_output=ff_output+bias
+        hidden_states = (hidden_states.float() + ff_output.float() * c_gate_msa).type_as(hidden_states)
+
+        return hidden_states
+    
+
+    def sharded_state_dict(
+        self, prefix: str = '', sharded_offsets: tuple = (), metadata: dict = None
+    ) -> ShardedStateDict:
+        """
+        Generate a sharded state dictionary for the transformer block.
+
+        Args:
+            prefix (str, optional): Prefix to be added to all keys in the state dict.
+                Defaults to an empty string.
+            sharded_offsets (tuple, optional): Tuple of sharding offsets.
+            metadata (dict, optional): Additional metadata for sharding.
+                Can specify if layers are non-homogeneous. Defaults to None.
+
+        Returns:
+            ShardedStateDict: A dictionary containing the sharded state of the model.
+        """
+        assert not sharded_offsets, "Unexpected sharded offsets"
+        non_homogeneous_layers = metadata is not None and metadata.get(
+            'non_homogeneous_layers', False
+        )
+        if self.config.num_moe_experts is not None:
+            non_homogeneous_layers = True
+
+        sharded_state_dict = {}
+
+        layer_prefix = f'{prefix}layers.'
+        num_layers = self.config.num_layers
+        for layer in self.layers:
+            offset = TransformerLayer._get_layer_offset(self.config)
+
+            global_layer_offset = layer.layer_number - 1  # self.layer_number starts at 1
+            state_dict_prefix = f'{layer_prefix}{global_layer_offset - offset}.'  # module list index in TransformerBlock # pylint: disable=line-too-long
+            if non_homogeneous_layers:
+                sharded_prefix = f'{layer_prefix}{global_layer_offset}.'
+                sharded_pp_offset = []
+            else:
+                sharded_prefix = layer_prefix
+                sharded_pp_offset = [
+                    (0, global_layer_offset, num_layers)
+                ]  # PP sharding offset for ShardedTensors
+            layer_sharded_state_dict = layer.sharded_state_dict(
+                state_dict_prefix, sharded_pp_offset, metadata
+            )
+            replace_prefix_for_sharding(layer_sharded_state_dict, state_dict_prefix, sharded_prefix)
+
+            sharded_state_dict.update(layer_sharded_state_dict)
+
+        # Add modules other than self.layers
+        for name, module in self.named_children():
+            if not module is self.layers:
+                sharded_state_dict.update(
+                    sharded_state_dict_default(
+                        module, f'{prefix}{name}.', sharded_offsets, metadata
+                    )
+                )
+
+        return sharded_state_dict
+
+def get_wan_spec() -> ModuleSpec:
+   return ModuleSpec(
+        module=WanDiTLayer,
+        submodules=TransformerLayerSubmodules(
+            self_attention=ModuleSpec(
+                module=WanSelfAttention,
+                params={"attn_mask_type": AttnMaskType.no_mask},
+                submodules=SelfAttentionSubmodules(
+                    linear_qkv=ColumnParallelLinear,
+                    core_attention=TEDotProductAttention,
+                    q_layernorm=RMSNorm,
+                    k_layernorm=RMSNorm,
+                    linear_proj=RowParallelLinear,
+                ),
+            ),
+            cross_attention=ModuleSpec(
+                module=WanCrossAttention,
+                submodules=WanCrossAttentionSubmodules(
+                    linear_q=ColumnParallelLinear,
+                    linear_k = ColumnParallelLinear,
+                    linear_v = ColumnParallelLinear,
+                    add_k_proj=ColumnParallelLinear,
+                    add_v_proj=ColumnParallelLinear,
+                    q_layernorm=RMSNorm,
+                    k_layernorm=RMSNorm,
+                    core_attention=TEDotProductAttention,
+                    linear_proj=RowParallelLinear,
+                    added_k_layernorm=RMSNorm,
+                ),
+            ),
+            mlp=ModuleSpec(
+                module=MLP,
+                submodules=MLPSubmodules(
+                    linear_fc1=ColumnParallelLinear,
+                    #dropout？dropout=0
+                    linear_fc2=RowParallelLinear,
+                ),
+            )
+        ),
+    )
---
+++ teletron/models/wan/model.py
@@ -0,0 +1,364 @@
+import os 
+from dataclasses import dataclass
+from megatron.core.transformer.utils import openai_gelu
+from typing import Callable,Any, Dict, List, Optional, Tuple, Union
+import torch.nn.functional as F
+import torch
+from megatron.core.models.common.vision_module.vision_module import VisionModule
+from megatron.core.transformer.transformer_config import TransformerConfig
+from megatron.training import get_args
+from diffusers.configuration_utils import ConfigMixin, register_to_config
+from diffusers.loaders import FromOriginalModelMixin, PeftAdapterMixin
+from diffusers.utils import USE_PEFT_BACKEND, logging, scale_lora_layers, unscale_lora_layers
+from diffusers.models.attention import FeedForward
+from diffusers.models.attention_processor import Attention
+from diffusers.models.cache_utils import CacheMixin
+from diffusers.models.embeddings import PixArtAlphaTextProjection, TimestepEmbedding, Timesteps, get_1d_rotary_pos_embed
+from diffusers.models.modeling_outputs import Transformer2DModelOutput
+from diffusers.models.modeling_utils import ModelMixin
+from diffusers.models.normalization import FP32LayerNorm
+from megatron.core.models.dit.wan_layerspec import (
+    WanDiTLayer,
+    get_wan_spec,
+)
+
+from megatron.core.models.wan.module import WanTimeTextImageEmbedding,WanRotaryPosEmbed,MLP
+
+from torch import nn
+import math
+from diffusers.utils import (
+    logging,
+)
+
+from einops import rearrange
+
+from megatron.core import mpu, tensor_parallel
+logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
+
+class WanParams:
+    patch_size: Tuple[int] = (1, 2, 2)
+    num_attention_heads: int = 40
+    attention_head_dim: int = 128
+    in_channels: int = 36
+    out_channels: int = 16
+    text_dim: int = 4096
+    freq_dim: int = 256
+    ffn_dim: int = 13824
+    num_layers: int = 5
+    cross_attn_norm: bool = True
+    qk_norm: Optional[str] = "rms_norm_across_heads"
+    eps: float = 1e-6
+    image_dim: int = 1280
+    added_kv_proj_dim: int = 5120
+    rope_max_seq_len: int = 1024
+
+class WanVideoTransformer3DModel(VisionModule):
+    def __init__(self, wan_config: WanParams, config: TransformerConfig):
+        self.out_channels = wan_config.out_channels
+        self.in_channels = wan_config.in_channels
+        self.num_attention_heads = wan_config.num_attention_heads
+        self.attention_head_dim = wan_config.attention_head_dim
+        self.patch_size = wan_config.patch_size
+        self.text_dim = wan_config.text_dim
+        self.freq_dim = wan_config.freq_dim
+        self.ffn_dim = wan_config.ffn_dim
+        self.cross_attn_norm = wan_config.cross_attn_norm
+        self.qk_norm = wan_config.qk_norm
+        self.eps = wan_config.eps
+        self.rope_max_seq_len=wan_config.rope_max_seq_len
+        self.patch_size=wan_config.patch_size
+        self.image_dim=wan_config.image_dim
+        self.added_kv_proj_dim = wan_config.added_kv_proj_dim
+        self.num_layers = wan_config.num_layers
+        args = get_args()
+        self.numer_layers = args.num_layers
+       
+        self.hidden_size = self.num_attention_heads * self.attention_head_dim
+        config.hidden_size =self.hidden_size
+        config.ffn_hidden_size = self.ffn_dim
+        config.num_attention_heads=self.num_attention_heads
+        # TODO: Assume not use GQA
+        config.num_query_groups = config.num_attention_heads
+        config.use_cpu_initialization = True
+        config.hidden_dropout=0
+        config.attention_dropout=0
+        config.layernorm_epsilon=1e-6
+        config.rotary_interleaved=True
+        config.attention_dropout = config.attention_dropout[0] if isinstance(config.attention_dropout, tuple) else config.attention_dropout
+        transformer_config=config
+
+        super().__init__(transformer_config)
+        self.inner_dim = self.num_attention_heads * self.attention_head_dim
+
+        out_channels = self.out_channels or self.in_channels
+
+        # 1. Patch & position embedding
+        # self.rope = WanRotaryPosEmbed(self.attention_head_dim, self.patch_size, self.rope_max_seq_len)
+
+        self.patch_embedding = nn.Conv3d(self.in_channels, self.inner_dim, kernel_size=self.patch_size, stride=self.patch_size)
+        
+        # 2. Condition embeddings
+        # # image_embedding_dim=1280 for I2V model
+        # self.condition_embedder = WanTimeTextImageEmbedding(
+        #     dim=self.inner_dim,
+        #     time_freq_dim=self.freq_dim,
+        #     time_proj_dim=self.inner_dim * 6,
+        #     text_embed_dim=self.text_dim,
+        #     image_embed_dim=self.image_dim,
+        # )
+
+        self.text_embedding = nn.Sequential(
+            nn.Linear(self.text_dim, self.inner_dim),
+            nn.GELU(approximate='tanh'),
+            nn.Linear(self.inner_dim, self.inner_dim)
+        )
+        self.time_embedding = nn.Sequential(
+            nn.Linear(self.freq_dim, self.inner_dim),
+            nn.SiLU(),
+            nn.Linear(self.inner_dim, self.inner_dim)
+        )
+        
+        self.time_projection = nn.Sequential(
+            nn.SiLU(), nn.Linear(self.inner_dim, self.inner_dim * 6))
+        
+        self.img_emb = MLP(self.image_dim, self.inner_dim)
+        self.freqs = precompute_freqs_cis_3d(self.attention_head_dim)
+
+        # 3. Transformer blocks
+        self.blocks = nn.ModuleList(
+            [
+                WanDiTLayer(
+                    config=transformer_config,
+                    submodules=get_wan_spec().submodules,
+                    layer_number=i,
+                )
+                for i in range(self.num_layers)
+            ]
+        )
+
+        # 4. Output norm & projection
+        self.norm_out = FP32LayerNorm(self.inner_dim, self.eps, elementwise_affine=False)
+        self.proj_out = nn.Linear(self.inner_dim, out_channels * math.prod(self.patch_size))
+        self.scale_shift_table = nn.Parameter(torch.randn(1, 2, self.inner_dim) / self.inner_dim**0.5)
+
+        print("Wan3DModel Init Finish!")
+    
+    def _get_block(
+            self,
+            layer_number: int
+    ):
+        return self.blocks[layer_number]
+
+    def _checkpointed_forward(
+        self, 
+        hidden_states: torch.Tensor,
+        encoder_hidden_states: torch.Tensor,
+        *args
+    ):
+        "Forward method with activation checkpointing."
+        recompute_layers = self.num_layers
+        def custom(start, end):
+            def custom_forward(*args):
+                for index in range(start, end):
+                    layer = self._get_block(index)
+                    x_ = layer(*args)
+                return x_
+            return custom_forward
+        if self.config.recompute_method == "uniform":
+            # Uniformly divide the total number of Transformer layers and
+            # checkpoint the input activation of each divided chunk.
+            # A method to further reduce memory usage reducing checkpoints.
+            _layer_num = 0
+            while _layer_num < self.num_layers:
+                hidden_states = tensor_parallel.checkpoint(
+                    custom(_layer_num, _layer_num + recompute_layers),
+                    self.config.distribute_saved_activations,
+                    hidden_states,
+                    encoder_hidden_states,
+                    *args
+                )
+                _layer_num += recompute_layers
+        elif self.config.recompute_method == "block":
+            # Checkpoint the input activation of only a set number of individual
+            # Transformer layers and skip the rest.
+            # A method fully use the device memory removing redundant re-computation.
+            if os.environ.get("PROFILE_MEMORY"):
+                from my_utils import ProfilerWrapper
+                profiler = ProfilerWrapper(is_st=False, enable_record_cuda_mm=True)
+            for _layer_num in range(recompute_layers):
+                if _layer_num < recompute_layers:
+                    hidden_states = tensor_parallel.checkpoint(
+                        custom(_layer_num, _layer_num + 1),
+                        self.config.distribute_saved_activations,
+                        hidden_states,
+                        encoder_hidden_states,
+                        *args
+                    )
+                else:
+                    block = self._get_block(_layer_num)
+                    hidden_states = block(*hidden_states, *encoder_hidden_states, *args)
+                if os.environ.get("PROFILE_MEMORY"):
+                    profiler.record()
+       
+        return  hidden_states
+
+    def patchify(self, x: torch.Tensor):
+        x = self.patch_embedding(x)
+        grid_size = x.shape[2:]
+        x = rearrange(x, 'b c f h w -> b (f h w) c').contiguous()
+        return x, grid_size  # x, grid_size: (f, h, w)
+
+    
+    def unpatchify(self, x: torch.Tensor, grid_size: torch.Tensor):
+        return rearrange(
+            x, 'b (f h w) (x y z c) -> b c (f x) (h y) (w z)',
+            f=grid_size[0], h=grid_size[1], w=grid_size[2], 
+            x=self.patch_size[0], y=self.patch_size[1], z=self.patch_size[2]
+        )
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        timestep: torch.LongTensor,
+        context: torch.Tensor,
+        clip_feature: Optional[torch.Tensor] = None,
+        y: Optional[torch.Tensor] = None,
+        cn_images=None, 
+        return_dict: bool = True,
+        attention_kwargs: Optional[Dict[str, Any]] = None,
+    ) -> Union[torch.Tensor, Dict[str, torch.Tensor]]:
+        if attention_kwargs is not None:
+            attention_kwargs = attention_kwargs.copy()
+            lora_scale = attention_kwargs.pop("scale", 1.0)
+        else:
+            lora_scale = 1.0
+
+        if USE_PEFT_BACKEND:
+            # weight the lora layers by setting `lora_scale` for each PEFT layer
+            scale_lora_layers(self, lora_scale)
+        else:
+            if attention_kwargs is not None and attention_kwargs.get("scale", None) is not None:
+                logger.warning(
+                    "Passing `scale` via `attention_kwargs` when not using the PEFT backend is ineffective."
+                )
+
+        t = self.time_embedding(
+            sinusoidal_embedding_1d(self.freq_dim, timestep))
+
+        t_mod = self.time_projection(t).unflatten(1, (6, -1))
+
+        context = self.text_embedding(context)
+
+        x = torch.cat([x, y], dim=1)  # (b, c_x + c_y, f, h, w)
+        clip_embdding = self.img_emb(clip_feature)
+        context = torch.cat([clip_embdding, context], dim=1)
+
+        if cn_images is not None:
+            x = torch.cat([x, cn_images], dim=1)  # (b, c_x + c_y, f, h, w)
+        
+        x, (f, h, w) = self.patchify(x)
+
+        freqs = torch.cat([
+            self.freqs[0][:f].view(f, 1, 1, -1).expand(f, h, w, -1),
+            self.freqs[1][:h].view(1, h, 1, -1).expand(f, h, w, -1),
+            self.freqs[2][:w].view(1, 1, w, -1).expand(f, h, w, -1)
+        ], dim=-1).reshape(f * h * w, 1, -1).to(x.device)
+
+        # if encoder_hidden_states_image is not None:
+        #     encoder_hidden_states = torch.concat([encoder_hidden_states_image, encoder_hidden_states], dim=1)
+        if mpu.get_context_parallel_world_size() > 1:
+            from megatron.core.tensor_parallel.mappings import (
+                split_forward_gather_backward,
+                gather_forward_split_backward,
+            )
+            x = split_forward_gather_backward(
+                x, 
+                mpu.get_context_parallel_group(),
+                dim=1,
+                grad_scale="down"
+            ) # b s n d
+     
+            freqs = split_forward_gather_backward(
+                freqs,
+                mpu.get_context_parallel_group(),
+                dim=0,
+                grad_scale="down"
+            )
+        # 4. Transformer blocks
+        if self.config.recompute_granularity == "full":
+            hidden_states = self._checkpointed_forward(
+                x,
+                context,
+                t_mod,
+                freqs
+            )
+        else:
+            if os.environ.get("PROFILE_MEMORY"):
+                from my_utils import ProfilerWrapper
+                profiler = ProfilerWrapper(is_st=False, enable_record_cuda_mm=True)
+
+            for block in self.blocks:
+                hidden_states = block(
+                    x,
+                    context,
+                    t_mod,
+                    freqs
+                )
+                if os.environ.get("PROFILE_MEMORY"):
+                    profiler.record()
+
+        if mpu.get_context_parallel_world_size() > 1:
+            hidden_states = gather_forward_split_backward(
+                hidden_states, 
+                mpu.get_context_parallel_group(),
+                dim=1,
+                grad_scale="up"
+            )
+        # 5. Output norm, projection & unpatchify
+        shift, scale = (self.scale_shift_table.to(t.device) + t.unsqueeze(1)).chunk(2, dim=1)
+
+        # # Move the shift and scale tensors to the same device as hidden_states.
+        # # When using multi-GPU inference via accelerate these will be on the
+        # # first device rather than the last device, which hidden_states ends up
+        # # on.
+        # shift = shift.to(hidden_states.device)
+        # scale = scale.to(hidden_states.device)
+
+        hidden_states = (self.norm_out(hidden_states.float()) * (1 + scale) + shift).type_as(hidden_states)
+        hidden_states = self.proj_out(hidden_states)
+
+        hidden_states = self.unpatchify(hidden_states, (f, h, w))
+        if not return_dict:
+            return (hidden_states,)
+
+        return Transformer2DModelOutput(sample=hidden_states)
+
+
+def precompute_freqs_cis_3d(dim: int, end: int = 1024, theta: float = 10000.0):
+    # 3d rope precompute
+    f_freqs_cis = precompute_freqs_cis(dim - 2 * (dim // 3), end, theta)
+    h_freqs_cis = precompute_freqs_cis(dim // 3, end, theta)
+    w_freqs_cis = precompute_freqs_cis(dim // 3, end, theta)
+    return f_freqs_cis, h_freqs_cis, w_freqs_cis
+
+def sinusoidal_embedding_1d(dim, position):
+    sinusoid = torch.outer(position.type(torch.float64), torch.pow(
+        10000, -torch.arange(dim//2, dtype=torch.float64, device=position.device).div(dim//2)))
+    x = torch.cat([torch.cos(sinusoid), torch.sin(sinusoid)], dim=1)
+    return x.to(position.dtype)
+
+def precompute_freqs_cis(dim: int, end: int = 1024, theta: float = 10000.0):
+    # 1d rope precompute
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)
+                   [: (dim // 2)].double() / dim))
+    freqs = torch.outer(torch.arange(end, device=freqs.device), freqs)
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
+    return freqs_cis
+
+
+def rope_apply(x, freqs, num_heads):
+    x = rearrange(x, "b s (n d) -> b s n d", n=num_heads)
+    x_out = torch.view_as_complex(x.to(torch.float64).reshape(
+        x.shape[0], x.shape[1], x.shape[2], -1, 2))
+    x_out = torch.view_as_real(x_out * freqs).flatten(2)
+    return x_out.to(x.dtype)
