a60d6f9da81817dc37be7affb51c1c39714a16db
FortPercent <fortpercent@users.noreply.github.com>
2025-06-07 19:02:27 +0800

    finish loss alignment with 1 blocks

--- test/test_scripts/multi_node.sh
+++ test/test_scripts/multi_node.sh
@@ -1,4 +1,8 @@
-export WORLD_SIZE=4
+
+# cp -r /nvfile-heatstorage/model_zoo/huggingface/hunyuan/hunyuanvideo_13b /workspace/
+# cp -r /nvfile-heatstorage/model_zoo/huggingface/hunyuan/hunyuanvideo_2p6b /workspace/
+
+export WORLD_SIZE=2
 export MASTER_ADDR=$GEMINI_HOST_IP_taskrole1_0
 export MASTER_PORT=12349
 export RANK=$GEMINI_CURRENT_TASK_ROLE_CURRENT_TASK_INDEX
