03b4e9413d2073bdafb8e1ac45881bf3726f28ef
FortPercent <fortpercent@users.noreply.github.com>
2025-06-11 12:28:01 +0800

    support save_path; support num_layers

--- teletron/convert_ckpt/wan/convert_wan.sh
+++ teletron/convert_ckpt/wan/convert_wan.sh
@@ -1,25 +1,21 @@
 export PYTHONPATH=
 export PYTHONPATH=$PYTHONPATH:/nvfile-heatstorage/yuc/teletron-wan/Megatron_wxe
-
 export PYTHONPATH=$PYTHONPATH:/nvfile-heatstorage/yxy/ccg/vast2
 export PYTHONPATH=$PYTHONPATH:/nvfile-heatstorage/teleai-infra/wxe/teleai_data_tool/
 # 
+TP=1
+PP=1
 
 HF_CKPT_PATH="/nvfile-heatstorage/model_zoo/huggingface/Wan2.1-I2V-14B-720P-Diffusers/transformer"
 SOURCE_CKPT_PATH="/nvfile-heatstorage/yxy/code/Teletron/debug/ckpt/origin/wan_prone10_step100"
 TARGET_CKPT_PATH="/nvfile-heatstorage/yxy/code/Teletron/debug/ckpt/prone_wan_tp1_pp1_layer_30_step100"
-TP=1
-PP=1
-
-
-# pip install -e /nvfile-heatstorage/teleai-infra/wxe/vast/
-# rsync -avh --ignore-existing --info=progress2 /nvfile-heatstorage/model_zoo/Wan2___1-I2V-14B-480P /workspace/
-# rsync -avh --ignore-existing --info=progress2 /nvfile-heatstorage/model_zoo/Wan2___1-FLF2V-14B-480P-init /workspace/
+mkdir $TARGET_CKPT_PATH
 
 python  convert_light_wan.py  \
     --hf-ckpt-path ${HF_CKPT_PATH} \
     --load ${SOURCE_CKPT_PATH} \
     --save ${TARGET_CKPT_PATH} \
+    --num-layers 30 \
     --target-params-dtype bf16 \
     --target-tensor-model-parallel-size ${TP} \
     --target-pipeline-model-parallel-size ${PP}
