408505f16f8bc7607dbbb4c3113c9f075154d030
FortPercent <fortpercent@users.noreply.github.com>
2025-06-12 09:39:53 +0800

    remove dependency of megatron-vast.

--- teletron/convert_ckpt/hunyuan/convert_hf2meg.sh
+++ teletron/convert_ckpt/hunyuan/convert_hf2meg.sh
@@ -1,29 +1,37 @@
-export PYTHONPATH=$PYTHONPATH:/nvfile-heatstorage/yxy/code/Megatron_VAST
+export PYTHONPATH=
+# TODO, change to your own path
+export PYTHONPATH=$PYTHONPATH:/nvfile-heatstorage/yxy/code/Megatron_060
 
-# Path to the HuggingFace checkpoint and its transformer directory
-CKPT_NAME="hunyuanvideo_i2vhy_token_replace"
-#CKPT_NAME="hunyuanvideo_i2v_multimask"
-
-HUGGINGFACE_CKPT_PATH="/nvfile-heatstorage/model_zoo/huggingface/hunyuan/hunyuanvideo_13b"
-CONFIG_FILE="/nvfile-heatstorage/yxy/code/Teletron/model_paths.json"
-CKPT_PATH=$(jq -r ".${CKPT_NAME}" "$CONFIG_FILE")
-SOURCE_CKPT_PATH="${CKPT_PATH}/transformer_safetensor"
-TARGET_CKPT_PATH="${CKPT_PATH}/teletron"
-
-WAN_VAE_PRETRAINED_PATH="/workspace/Wan2.1-I2V-14B-720P-Diffusers/vae" 
 # Model parallelism settings
 TP=1  # Tensor Parallelism
 PP=1  # Pipeline Parallelism
 
+# Path to the HuggingFace checkpoint and its transformer directory
+HUGGINGFACE_CKPT_PATH="/nvfile-heatstorage/model_zoo/huggingface/hunyuan/hunyuanvideo_13b"
+SOURCE_CKPT_PATH="/nvfile-heatstorage/hyc/vast/work_dirs/hunyuanvideo_i2vhy_sp2_720p_85_24fps_0512/models/checkpoint_epoch_1_step_5000/transformer_safetensor"
+TARGET_CKPT_PATH="/nvfile-heatstorage/hyc/vast/work_dirs/hunyuanvideo_i2vhy_sp2_720p_85_24fps_0512/models/checkpoint_epoch_1_step_5000/teletron_debug"
 rm -r $TARGET_CKPT_PATH
 
+
 # Run the conversion script
-# python convert_hunyuanvideo.py  \
-python convert_hunyuanvideo_t2i.py  \
+
+### convert hunyuanvideo
+python convert_hunyuanvideo.py  \
     --load ${SOURCE_CKPT_PATH} \
     --save ${TARGET_CKPT_PATH} \
     --hf-ckpt-path ${HUGGINGFACE_CKPT_PATH} \
-    --wan_vae_pretrained_path ${WAN_VAE_PRETRAINED_PATH} \
     --target-params-dtype bf16 \
     --target-tensor-model-parallel-size ${TP} \
     --target-pipeline-model-parallel-size ${PP}
+
+
+### convert hunyuanvideo with wanvae
+# WAN_VAE_PRETRAINED_PATH="/workspace/Wan2.1-I2V-14B-720P-Diffusers/vae" 
+# python convert_hunyuanvideo_t2i.py  \
+#     --load ${SOURCE_CKPT_PATH} \
+#     --save ${TARGET_CKPT_PATH} \
+#     --hf-ckpt-path ${HUGGINGFACE_CKPT_PATH} \
+#     --wan_vae_pretrained_path ${WAN_VAE_PRETRAINED_PATH} \
+#     --target-params-dtype bf16 \
+#     --target-tensor-model-parallel-size ${TP} \
+#     --target-pipeline-model-parallel-size ${PP}
