00ae9f36d69fb9e62720dfffc35205b798dfd3e0
FortPercent <fortpercent@users.noreply.github.com>
2025-06-24 17:52:42 +0800

    fix bugs for none activated venv clean import code and move dataset and dateloader code to proper place

--- teletron/datasets/build.py
+++ teletron/datasets/build.py
@@ -3,6 +3,27 @@
 from .registry import Registry, build_module
 from .fake_dataset import FakeDataset
 from .koala_dataset import KoalaDataset
+import torch
+from megatron.core import mpu
+from vast.train.samplers import build_sampler as build_sampler_vast
+from vast.datasets import DefaultCollator
+from vast.datasets.datasets.build import build_dataset as build_dataset_vast
+from teletron.utils import (
+    print_rank_0,
+    get_args,
+)
+from teletron.train.utils import (
+    set_config,
+    get_train_valid_test_num_samples,
+)
+from teletron.datasets.vast_dataset.hunyuan_dataset_config import (
+    HunyuanVideoDatasetConfig,
+)
+from teletron.datasets.vast_dataset.hunyuanvideo_dataset_builder import (
+    HunyuanVideoDatasetBuilder,
+)
+
+
 
 DATASETS = Registry()
 DATASETS.register_module(FakeDataset)
@@ -11,3 +32,84 @@ DATASETS.register_module(KoalaDataset)
 
 def build_dataset(params_or_type, *args, **kwargs):
     return build_module(DATASETS, params_or_type, *args, **kwargs)
+
+
+def build_train_valid_test_datasets(dp_rank=None, dp_size=None):
+    """Build pretraining datasets."""
+    train_valid_test_num_samples = get_train_valid_test_num_samples()
+    print_rank_0(' > datasets target sizes (minimum size):')
+    print_rank_0('    train:      {}'.format(train_valid_test_num_samples[0]))
+    print_rank_0('    validation: {}'.format(train_valid_test_num_samples[1]))
+    print_rank_0('    test:       {}'.format(train_valid_test_num_samples[2]))
+    args = get_args()
+
+    print_rank_0("> building train, validation, and test datasets for multimodal ...")
+
+    if args.dataset_type == "FakeDataset" or args.dataset_type == "KoalaDataset":
+        train_ds = build_dataset(args.dataset_type)
+        valid_ds = None
+        test_ds = None
+    elif args.dataset_type == "VastDataset": 
+        global_config = set_config()
+        train_ds_config = global_config.dataloaders.train
+        eval_ds_config = global_config.dataloaders.get("eval", None)
+        ds_config = HunyuanVideoDatasetConfig(
+            train_ds_config=train_ds_config,
+            eval_ds_config=eval_ds_config
+        )
+        dataset = build_dataset_vast(train_ds_config.dataset)
+        train_ds, valid_ds, test_ds = HunyuanVideoDatasetBuilder(
+            dataset,
+            train_valid_test_num_samples,
+            lambda: True,
+            ds_config,
+        ).build()
+
+    elif args.dataset_type == "BucketDataset": 
+        global_config = set_config()
+        assert global_config.sampler.type == "BucketVariableBatchSampler"
+        assert args.dataloader_type == 'external', "BucketDataset use cumstomed dataloader"
+        assert args.task_type == "wan_i2v_bucket", "BucketDataset is only supported for t2i_wanvae task"
+        print_rank_0("Warning: The `args.micro_batch_size` and `seed` from vast dataset config will NOT BE USED when use BucketDataset.")
+
+        dataset = build_dataset_vast(global_config.dataset)
+        if dp_rank is None or dp_size is None:
+            sampler = build_sampler_vast(
+                global_config.sampler,
+                dataset=dataset, 
+                rank=mpu.get_data_parallel_rank(),
+                num_replicas=mpu.get_data_parallel_world_size(),
+                seed=args.seed
+            )
+        else:
+            sampler = build_sampler_vast(
+                global_config.sampler,
+                dataset=dataset, 
+                rank=dp_rank,
+                num_replicas=dp_size,
+                seed=args.seed
+            )
+        # 使用 iteration 和 num_replicas / world size 计算，近似最后访问的索引
+        if args.last_microbatch_size_index is None:
+            sampler.last_micro_batch_access_index = args.iteration * sampler.num_replicas
+        else:
+            sampler.last_micro_batch_access_index = args.last_microbatch_size_index
+        # else:
+        # sampler.last_micro_batch_access_index = iteration * sampler.num_replicas
+
+        collator = DefaultCollator(is_equal=True)
+        train_ds = torch.utils.data.DataLoader(
+            dataset,
+            batch_sampler=sampler,
+            collate_fn=collator,
+            num_workers=global_config.dataloader.num_workers
+        )
+        train_ds = iter(train_ds)
+        valid_ds = None
+        test_ds = None
+    else:
+        raise NotImplementedError
+
+    print_rank_0("> finished creating multimodal datasets ...")
+
+    return train_ds, valid_ds, test_ds
