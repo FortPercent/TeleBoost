f62b22cefddcc9f5242dd362ad8979b3e5d5c801
liukaikai152 <night@mail.ustc.edu.cn>
2025-08-11 16:40:53 +0800

    clean infer

--- examples/inference/causal_pipeline/_init__.py
+++
@@ -1,8 +0,0 @@
-
-from .causal_inference import CausalInferencePipeline
-from .casual_fps_inference import CausalFPSInferencePipeline
-
-__all__ = [
-    "CausalInferencePipeline",
-    "CausalFPSInferencePipeline"
-]
