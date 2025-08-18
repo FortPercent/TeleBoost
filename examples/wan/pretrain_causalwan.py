6243f6e968d169bd97c43f17297db1d772d403cc
liukaikai152 <night@mail.ustc.edu.cn>
2025-08-18 15:50:46 +0800

    clean #

--- examples/wan/pretrain_causalwan.py
+++ examples/wan/pretrain_causalwan.py
@@ -20,17 +20,13 @@ def causal_loss_func(output_tensor):
 def extra_args(parser):
     group = parser.add_argument_group(title='customized args')
     # follow this format to add
-    group.add_argument("--no_save", action="store_false")
-    group.add_argument("--load_raw_video", action="store_false")
-    group.add_argument("--gradient-checkpointing", action="store_false")
-    group.add_argument("--real-name", type=str, default="Wan2.1-T2V-14B")
+    group.add_argument("--real-name", type=str, default="Wan2.1-T2V")
     group.add_argument("--negative_prompt",type=str,
                        default="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走")
     group.add_argument("--timestep_shift",default=5.0)
     group.add_argument("--model_kwargs",default={"timestep_shift": 5.0})
     group.add_argument("--guidance_scale",default=5.0)
     group.add_argument("--mixed_precision",default=True)
-    group.add_argument("--sink",default=False)
     
     group2 = parser.add_argument_group(title='encoder args')
     group2.add_argument("--encoder_model_path", type=str, nargs = '+',default=
@@ -40,15 +36,6 @@ def extra_args(parser):
                        )
     group2.add_argument("--encoder_tokenizer_path", type=str, default=
                        "/workspace/Wan2___1-I2V-14B-480P/google/umt5-xxl")
-    # group.add_argument("--base_paths", type=str, nargs = '+',
-    #                    default=['/nvfile-heatstorage/teleai-infra/kaikai/HumanData_subset_500/merged_videos_latents',]
-    #                    )
-    # group.add_argument("--metadata_paths", type=str, nargs = '+',
-    #                    default=['/nvfile-heatstorage/teleai-infra/kaikai/HumanData_subset_500/filtered_500.csv',]
-    #                    )
-    # group.add_argument("--logdir", type=str, default="wan_experiments_test", help="Path to the directory to save logs")
-    # group.add_argument("--test_valid", type=str, default="")
-    # group.add_argument("--moe-step-factor-list", type=float, action='append')
 
     return parser
 
@@ -71,7 +58,6 @@ def forward_step(data_iterator, ddpmodel):
     clean_latent = batch["latents"].to(device, dtype)
     clean_latent = clean_latent.permute(0, 2, 1, 3, 4)
     image_latent = clean_latent[:, 0:1, ]
-    batch_size = args.micro_batch_size
     image_or_video_shape = args.image_or_video_shape
 
     from teletron.utils.aux_func import get_attr_wrapped_model
@@ -79,11 +65,6 @@ def forward_step(data_iterator, ddpmodel):
         conditional_dict = {'prompt_embeds':batch["prompt_emb"]}
         if not getattr(ddpmodel, "unconditional_dict", None):
             unconditional_dict = {'prompt_embeds':batch["unprompt_emb"]}
-            # text_encoder = get_attr_wrapped_model(ddpmodel,"text_encoder")
-            # unconditional_dict = text_encoder(
-            #     text_prompts=[args.negative_prompt] * batch_size)
-            # unconditional_dict = {k: v.detach() for k, v in unconditional_dict.items()}
-            # ddpmodel.unconditional_dict = unconditional_dict
         else:
             unconditional_dict = ddpmodel.unconditional_dict
 
@@ -100,48 +81,10 @@ def forward_step(data_iterator, ddpmodel):
     
     return [generator_loss, generator_loss_w], causal_loss_func
 
-# def forward_step2(data_iterator, ddpmodel):
-#     batch = next(data_iterator)
-#     args = get_args()
-#     device = torch.cuda.current_device()
-#     dtype = torch.bfloat16 if args.mixed_precision else torch.float32
-#     accumulation_steps = getattr(ddpmodel, "accumulation_steps", 1)
-    
-#     clean_latent = batch["latents"].to(device, dtype)
-#     clean_latent = clean_latent.permute(0, 2, 1, 3, 4)
-#     image_latent = clean_latent[:, 0:1, ]
-#     image_or_video_shape = args.image_or_video_shape
-
-#     from teletron.utils.aux_func import get_attr_wrapped_model
-#     with torch.no_grad():
-#         conditional_dict = {'prompt_embeds':batch["prompt_emb"][0]}
-#         if not getattr(ddpmodel, "unconditional_dict", None):
-#             unconditional_dict = {'prompt_embeds':batch["unprompt_emb"][0]}
-#             ddpmodel.unconditional_dict = unconditional_dict
-#         else:
-#             unconditional_dict = ddpmodel.unconditional_dict
-#     print(f'conditional_dict:{conditional_dict["prompt_embeds"].shape}')
-#     print(f'unconditional_dict:{unconditional_dict["prompt_embeds"].shape}')
-#     print(f'clean_latent:{clean_latent.shape}')
-#     generator_loss = get_attr_wrapped_model(ddpmodel,"generator_loss")
-#     generator_loss, generator_loss_w, log_dict = generator_loss(
-#         image_or_video_shape=image_or_video_shape,
-#         conditional_dict=conditional_dict,
-#         unconditional_dict=unconditional_dict,
-#         clean_latent=clean_latent,
-#         initial_latent=image_latent,
-#     )
-#     generator_loss = generator_loss / accumulation_steps
-#     generator_loss_w = generator_loss_w / accumulation_steps
-    
-#     return [generator_loss, generator_loss_w], loss_func
 
 if __name__ == "__main__":
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
     args = parse_args(extra_args=extra_args)
-    # if use ode_init：
-    # args.generator_ckpt = '/nvfile-heatstorage/teleai-infra/kaikai/checkpoints/ode_init.pt'
-    # args.distributed_vae = False
     trainer = Trainer(args)
     trainer.pretrain(forward_step_func=forward_step)
