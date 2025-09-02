cae415b342f30da46b369bdc2cf10003e82d9bbd
xywang <mmy360@firefox.com>
2025-09-02 18:52:03 +0800

    test

--- recipe/dancegrpo/dancegrpo_ray_trainer.py
+++ recipe/dancegrpo/dancegrpo_ray_trainer.py
@@ -1,21 +1,3 @@
-# Copyright 2024 Bytedance Ltd. and/or its affiliates
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""
-FSDP PPO Trainer with Ray-based single controller.
-This trainer supports model-agonistic model initialization with huggingface
-"""
-
 import uuid
 from collections import defaultdict
 from copy import deepcopy
@@ -211,6 +193,9 @@ class RayDanceGRPOTrainer(RayPPOTrainer):
                             with torch.amp.autocast('cuda'):
                                 # TODO:关键要修改这里！
                                 reward_tensor = self.rm_wg.compute_rm_score(gen_batch_output)
+                                reward = reward_tensor.pop(batch_keys=['rewards'],).batch['rewards'].mean()
+                                fprint(reward)
+                                fprint(reward_tensor.batch["rewards"])                                
                                 new_batch = gen_batch_output.union(reward_tensor)
                                 new_batch.pop(batch_keys=['video_frames'])
                                 del gen_batch_output
