d059e868fb6d74bc11615c7c804011bcf26c987d
FortPercent <fortpercent@users.noreply.github.com>
2026-01-05 22:51:29 +0800

    update diffsynth dpo dataset and model

--- teletron/models/wan/__init__.py
+++ teletron/models/wan/__init__.py
@@ -1,2 +1,3 @@
-from .wan_model import WanModel
+from .wan_model import WanModel, sinusoidal_embedding_1d
 from .parallel_wan_model import ParallelWanModel
+from .dpo_wan_model import WanTrainingModule
