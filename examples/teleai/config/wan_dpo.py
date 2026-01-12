ee279438abf16746f1df71cb2bcb37cb7e1ed86e
papa <2215517664@qq.com>
2026-01-12 11:04:56 +0800

    update

--- examples/teleai/config/wan_dpo.py
+++ examples/teleai/config/wan_dpo.py
@@ -22,10 +22,11 @@ config = dict(
         rejected_video_key="rejected",
 
         # === 视频尺寸 & 时序 ===
-        # height=720,
-        height=480,
-        width=832,
-        num_frames=49,
+        height=720,
+        # height=480,
+        # width=832,
+        width=1280,
+        num_frames=81,
 
         # === 这些原脚本是写死的，现在 config 化 ===
         time_division_factor=4,
@@ -92,13 +93,13 @@ config = dict(
                 has_image_input=True, # t2v:False i2v:True i2v Wan2.2:False
                 patch_size=[1, 2, 2],
                 in_dim=36, # t2v:16 i2v:36
-                dim=1536, # 1.3B:1536 10B:5120 14B:5120
-                ffn_dim=8960, # 1.3B:8960 10B:13824 14B:13824
+                dim=5120, # 1.3B:1536 10B:5120 14B:5120
+                ffn_dim=13824, # 1.3B:8960 10B:13824 14B:13824
                 freq_dim=256,
                 text_dim=4096,
                 out_dim=16,
-                num_heads=12, # 1.3B:12 10B:40 14B:40
-                num_layers=30, # 1.3B:30 10B:30 14B:40
+                num_heads=40, # 1.3B:12 10B:40 14B:40
+                num_layers=40, # 1.3B:30 10B:30 14B:40
                 eps=1e-6,
                 has_image_pos_emb=False, 
             ),
