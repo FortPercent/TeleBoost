80412863a356f012b90f8e5b2ca14d69c5d8e61a
papa <2215517664@qq.com>
2026-01-20 14:02:47 +0800

    debug

--- examples/teleai/run_dpo_dataset_pipeline_test.sh
+++ examples/teleai/run_dpo_dataset_pipeline_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-NPROC_PER_NODE="${NPROC_PER_NODE:-2}"
+NPROC_PER_NODE="${NPROC_PER_NODE:-1}"
 
 export PYTHONPATH="${PYTHONPATH:-}:/nvfile-heatstorage/ai_infra/code/lit117/Megatron-LM"
 export PYTHONPATH="${PYTHONPATH}:/nvfile-heatstorage/ai_infra/code/lit117/yuc/env/teleai_data_tool"
