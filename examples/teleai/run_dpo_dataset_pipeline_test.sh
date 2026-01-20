777c51d8539efe4f4001e888ab862a67c2c48e5f
yp <2215517664@qq.com>
2026-01-20 12:15:58 +0800

    update test_dpo_dataset demo

--- examples/teleai/run_dpo_dataset_pipeline_test.sh
+++ examples/teleai/run_dpo_dataset_pipeline_test.sh
@@ -7,7 +7,6 @@ export PYTHONPATH="${PYTHONPATH:-}:/nvfile-heatstorage/ai_infra/code/lit117/Mega
 export PYTHONPATH="${PYTHONPATH}:/nvfile-heatstorage/ai_infra/code/lit117/yuc/env/teleai_data_tool"
 export PYTHONPATH="${PYTHONPATH}:/nvfile-heatstorage/ai_infra/code/lit117/qiuyang/Video-Depth-Anything/"
 
-torchrun --standalone --nproc_per_node "${NPROC_PER_NODE}" -m pytest \
-    tests/unit_tests/test_dpo_dataset_pipeline_torchrun.py \
-    -o log_cli=true -o log_cli_level=INFO \
+torchrun --standalone --nproc_per_node "${NPROC_PER_NODE}" \
+    tests/unit_tests/run_dpo_dataset_pipeline_torchrun.py \
     "$@"
