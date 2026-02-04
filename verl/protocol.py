bad74d7e5e841a445e7ffdcd13b02ff1327426e3
hiahei <850405207@qq.com>
2026-02-04 14:11:57 +0800

    False的情况会出现protocol传数据的格式问题，传入的是5D视频数组

--- verl/protocol.py
+++ verl/protocol.py
@@ -120,7 +120,12 @@ def union_numpy_dict(tensor_dict1: dict[str, np.ndarray], tensor_dict2: dict[str
             assert isinstance(tensor_dict2[key], np.ndarray)
             assert isinstance(tensor_dict1[key], np.ndarray)
             # to properly deal with nan and object type
-            assert pd.DataFrame(tensor_dict2[key]).equals(pd.DataFrame(tensor_dict1[key])), f"{key} in tensor_dict1 and tensor_dict2 are not the same object"
+            arr2 = tensor_dict2[key]
+            arr1 = tensor_dict1[key]
+            if arr1.ndim <= 2 and arr2.ndim <= 2:
+                assert pd.DataFrame(arr2).equals(pd.DataFrame(arr1)), f"{key} in tensor_dict1 and tensor_dict2 are not the same object"
+            else:
+                assert np.array_equal(arr2, arr1), f"{key} in tensor_dict1 and tensor_dict2 are not the same object"
         tensor_dict1[key] = val
 
     return tensor_dict1
